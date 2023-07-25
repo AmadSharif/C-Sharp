0ac89cf6611dcf55f87de8c53197c79caa3b0de0
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-07-25 11:40:22 -0700

    Create multiplication table of any number.cs
    user input and for-loop practice

---
+++ multiplication table of any number.cs
@@ -0,0 +1,16 @@
+﻿
+
+public class Program
+{
+     static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Enter Number");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Multiplication Table of"+""+n);
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(n+"x{0} = {1}", i, n * i);
+        }
+    }
+} //("n * {0} = {1}", i, n * i);
