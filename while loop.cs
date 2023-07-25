ccff4f7379de1087e3118e0878f859e598e15ad2
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-07-25 13:01:25 -0700

    Create while loop.cs
    Table of any number using while-loop

---
+++ while loop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+     static void Main(string[] args)
+    {
+        int valueOne;
+        int valueTwo=1;
+        
+        Console.WriteLine("Enter Number");
+        valueOne = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Multiplication Table of"+" "+valueOne);
+        while(valueTwo <= 20)
+        {
+            Console.WriteLine(valueOne+" x " +valueTwo+ " = "+(valueOne*valueTwo));
+            valueTwo++;
+        }
+    }
+}
+
