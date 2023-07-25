b534845d0e0212e239091fdad02aa418760f6030
HammadMaqbool <hammad.mqbl@gmail.com>
2023-07-25 23:55:41 +0500

    Update multiplication table of any number.cs
    1. Meaning full variable name usage. 
    2. Change the output line no need to use place holders.
    3. Using System added on top

--- multiplication table of any number.cs
+++ multiplication table of any number.cs
@@ -1,16 +1,16 @@
-﻿
-
+using System;
 public class Program
 {
      static void Main(string[] args)
     {
-        int n;
+        int valueOne;
         Console.WriteLine("Enter Number");
-        n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Multiplication Table of"+""+n);
+        valueOne = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Multiplication Table of"+""+valueOne);
         for (int i = 1; i <= 10; i++)
         {
-            Console.WriteLine(n+"x{0} = {1}", i, n * i);
+            Console.WriteLine(valueOne+" x "+i+" = "+(valueOne*i));
         }
     }
 }
+
