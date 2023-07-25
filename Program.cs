7c9ace3355b8e809c4122940c9e206c79da1f2d5
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-07-25 03:16:07 -0700

    writeline and readline use
    variable declaration and initialization

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main()
+        {
+          string firstName;
+          string lastName;
+
+          Console.WriteLine("enter first name");
+          firstName=Console.ReadLine();
+
+          Console.WriteLine("enter last name");
+          lastName=Console.ReadLine();
+
+          Console.WriteLine("full Name is "+ firstName+" "+lastName);
+
+
+        }
+    }
+}
